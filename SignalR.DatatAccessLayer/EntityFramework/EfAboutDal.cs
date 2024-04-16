3e943022b1c40607f39ace0fb70262b3b275c34f
habibesayrac <habibesayrac@outlook.com>
2024-04-16 11:38:49 +0300

    DataAccessLayer Abstract created

---
+++ SignalR.DatatAccessLayer/EntityFramework/EfAboutDal.cs
@@ -0,0 +1,38 @@
+﻿using SignalR.DatatAccessLayer.Abstract;
+using SignalRApi.EntityLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignalR.DatatAccessLayer.EntityFramework
+{
+    public class EfAboutDal : IAboutDal
+    {
+        public void Add(About entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(About entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public About GetByID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<About> GetListAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(About entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
