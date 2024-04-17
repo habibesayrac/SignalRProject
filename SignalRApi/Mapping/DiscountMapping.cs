f57c933f05e38a90242590c0808626ad2446bf02
habibesayrac <habibesayrac@outlook.com>
2024-04-17 09:22:46 +0300

    Mapping added

---
+++ SignalRApi/Mapping/DiscountMapping.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using SignalR.DtoLayer.DiscountDto;
+
+namespace SignalRApi.Mapping
+{
+    public class DiscountMapping:Profile
+    {
+        public DiscountMapping()
+        {
+            CreateMap<DiscountMapping, ResultDiscountDto>().ReverseMap();
+            CreateMap<DiscountMapping, CreateDiscountDto>().ReverseMap();
+            CreateMap<DiscountMapping, UpdateDiscountDto>().ReverseMap();
+            CreateMap<DiscountMapping, GetDiscountDto>().ReverseMap();
+        }
+    }
+}
