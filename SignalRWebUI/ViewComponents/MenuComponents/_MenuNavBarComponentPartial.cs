fab6b05edbc3d515487c37232d37883d5f3f5237
habibesayrac <habibesayrac@outlook.com>
2024-05-16 11:46:22 +0300

    Home &Menu  Added

---
+++ SignalRWebUI/ViewComponents/MenuComponents/_MenuNavBarComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SignalRWebUI.ViewComponents.MenuComponents
+{
+    public class _MenuNavBarComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
--- SignalRWebUI/ViewComponents/UILayoutComponents/_IULayoutFooterComponentPartial.cs
+++ SignalRWebUI/ViewComponents/UILayoutComponents/_IULayoutFooterComponentPartial.cs
@@ -1,12 +1,26 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SignalRWebUI.Dtos.ContactDtos;
 
 namespace SignalRWebUI.ViewComponents.UILayoutComponents
 {
     public class _IULayoutFooterComponentPartial:ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _IULayoutFooterComponentPartial(IHttpClientFactory httpClientFactory)
         {
-            return View();
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7277/api/Contact");
+            var jsonData = await responseMessage.Content.ReadAsStringAsync();
+            var values = JsonConvert.DeserializeObject<List<ResultContactDto>>(jsonData);
+            return View(values);
+
         }
     }
 }
