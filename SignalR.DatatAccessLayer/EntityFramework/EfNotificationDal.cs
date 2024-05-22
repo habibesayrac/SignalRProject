2e122155fbbeb2a7856f92d1226fe1aca0f6f034
habibesayrac <habibesayrac@outlook.com>
2024-05-22 12:06:49 +0300

    Notification Entity Added

---
+++ SignalR.DatatAccessLayer/EntityFramework/EfNotificationDal.cs
@@ -0,0 +1,19 @@
+﻿using SignalR.DatatAccessLayer.Abstract;
+using SignalR.DatatAccessLayer.Concrete;
+using SignalR.DatatAccessLayer.Repositories;
+using SignalR.EntityLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignalR.DatatAccessLayer.EntityFramework
+{
+    public class EfNotificationDal : GenericRepository<Notification>, INotificationDal
+    {
+        public EfNotificationDal(SignalRContext context) : base(context)
+        {
+        }
+    }
+}
