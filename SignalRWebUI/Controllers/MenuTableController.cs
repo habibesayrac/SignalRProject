921a259dd13a059db4ef0ca4ba2e43b98c544002
habibesayrac <habibesayrac@outlook.com>
2024-05-27 18:28:35 +0300

    menu table status added

--- SignalRWebUI/Controllers/MenuTableController.cs
+++ SignalRWebUI/Controllers/MenuTableController.cs
@@ -87,5 +87,19 @@ namespace SignalRWebUI.Controllers
             return View();
 
         }
+        [HttpGet]
+        public async Task<IActionResult> TableListByStatus()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7277/api/MenuTables");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultMenuTableDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+
     }
 }
