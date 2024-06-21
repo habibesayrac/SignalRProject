142989761d48581448c282cdfba71a874c9c3e56
habibesayrac <habibesayrac@outlook.com>
2024-06-21 20:37:11 +0300

    ProgressBar Added

--- SignalRApi/Hubs/SignalRHub.cs
+++ SignalRApi/Hubs/SignalRHub.cs
@@ -86,6 +86,15 @@ namespace SignalRApi.Hubs
 
 			var value3 = _menuTableService.TMenuTableCount();
 			await Clients.All.SendAsync("ReceiveMenuTableCount", value3);
+            
+            var value5 = _productService.TProductPriceAvg();
+			await Clients.All.SendAsync("ReceiveProductPriceAvg", value5);
+
+            var value6 = _productService.TProductAvgPriceByHamburger();
+			await Clients.All.SendAsync("ReceiveProductAvgPriceByHamburger", value6);
+            
+            var value7= _productService.TProductCountByCategoryNameDrink();
+			await Clients.All.SendAsync("ReceiveProductCountByCategoryNameDrink", value7);
 		}
 
         public async Task GetBookingList()
